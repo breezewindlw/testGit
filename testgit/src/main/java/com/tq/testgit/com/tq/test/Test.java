a5834f79c17c62cbeb9be8dfd10109445a8e22e4
lw <595899530@qq.com>
2019-03-20 14:13:50 +0800

    test

--- testgit/src/main/java/com/tq/testgit/com/tq/test/Test.java
+++ testgit/src/main/java/com/tq/testgit/com/tq/test/Test.java
@@ -9,5 +9,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
         System.out.println("test git");
+        System.out.println("test github");
     }
 }
