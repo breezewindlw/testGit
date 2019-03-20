83a5781e4583c5891e952a72b94c866142fab098
lw <595899530@qq.com>
2019-03-20 14:05:18 +0800

    test

---
+++ testgit/src/main/java/com/tq/testgit/com/tq/test/Test.java
@@ -0,0 +1,12 @@
+package com.tq.testgit.com.tq.test;
+
+/**
+ * @author liuwei
+ * @create 2019-03-20 14:04
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
